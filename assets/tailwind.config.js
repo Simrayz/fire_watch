fb15b36719f65febb48bed17635b57dc5c8993e3
Simen Abelsen <simenabelsen@hotmail.no>
2021-02-08 14:18:39 +0100

    Setup tailwind for use with phoenix

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    '../lib/**/*.ex',
+    '../lib/**/*.leex',
+    '../lib/**/*.eex',
+    './js/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {},
+  plugins: []
+};
