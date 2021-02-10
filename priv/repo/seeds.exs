98adfd0a68fa73b16ea5ce97f2ec23ac4df818ab
Simen Abelsen <simenabelsen@hotmail.no>
2021-02-10 02:29:36 +0100

    Add seed functions to seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias FireWatch.Fires.CSV
+
+CSV.import_fires("forestfires.csv")
+|> CSV.convert_fires()
+|> CSV.batch_insert()
